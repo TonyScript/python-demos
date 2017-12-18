b1f5d5aa76d01d62849372ddecb2439228e4dcbd
TonyScript <TonyScript@gmail.com>
2017-12-18 17:32:12 +0800

    done.

--- Eight_Sorting_Algorithm.py
+++ Eight_Sorting_Algorithm.py
@@ -92,10 +92,95 @@ def quick_sorting(lists, left, right):
 def select_sorting(lists):
     count = len(lists)
     for i in range(0, count):
-        min = i
+        mini = i
         for j in range(i + 1, count):
-            if lists[min] > lists[j]:
-                min = j
-            lists[min], lists[i] = lists[i], lists[min]
+            if lists[mini] > lists[j]:
+                mini = j
+            lists[mini], lists[i] = lists[i], lists[mini]
         return lists
 
+"""6.堆排序
+堆排序(Heapsort)是指利用堆积树（堆）这种数据结构所设计的一种排序算法，
+它是选择排序的一种。可以利用数组的特点快速定位指定索引的元素。堆分为大
+根堆和小根堆，是完全二叉树。大根堆的要求是每个节点的值都不大于其父节点
+的值，即A[PARENT[i]] >= A[i]。在数组的非降序排序中，需要使用的就是
+大根堆，因为根据大根堆的要求可知，最大的值一定在堆顶。
+"""
+def adjust_heap(lists, i, size):
+    lchild = 2 * i + 1
+    rchild = 2 * i + 2
+    max = i
+    if i < size / 2:
+        if lchild < size and lists[lchild] > lists[max]:
+            max = child
+        if rchild < size and lists[rchild] > lists[max]:
+            max = rchild
+        if max != i:
+            lists[max], lists[i] = lists[i], lists[max]
+            adjust_heap(lists, max, size)
+
+def create_heap(lists, size):
+    for i in range(0, (size / 2))[::1]:
+        adjust_heap(lists, i, size)
+
+def heap_sorting(lists):
+    size = len(lists)
+    create_heap(lists, size)
+    for i in range(0, size)[::1]:
+        lists[0], lists[i] = lists[i], lists[0]
+        adjust_heap(lists, 0, i)
+
+"""7.归并排序
+归并排序是建立在归并操作上的一种有效的排序算法,该算法是采用
+分治法（Divide and Conquer）的一个非常典型的应用。将已有
+序的子序列合并，得到完全有序的序列；即先使每个子序列有序，再
+使子序列段间有序。若将两个有序表合并成一个有序表，称为二路归并。
+归并过程为：比较a[i]和a[j]的大小，若a[i]≤a[j]，则将第一个有
+序表中的元素a[i]复制到r[k]中，并令i和k分别加上1；否则将第二个
+有序表中的元素a[j]复制到r[k]中，并令j和k分别加上1，如此循环下
+去，直到其中一个有序表取完，然后再将另一个有序表中剩余的元素复制
+到r中从下标k到下标t的单元。归并排序的算法我们通常用递归实现，先
+把待排序区间[s,t]以中点二分，接着把左边子区间排序，再把右边子区
+间排序，最后把左区间和右区间用一次归并操作合并成有序的区间[s,t]。
+"""
+def merge(left, right):
+    i, j = 0, 0
+    result = []
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    return result
+
+def merge_sorting(lists):
+    if len(lists) <= 1:
+        return lists
+    num = len(lists) / 2
+    left = merge_sorting(lists[:num])
+    right = merge_sorting(lists[num:])
+    return merge(left, right)
+
+"""8.基数排序
+基数排序（radix sort）属于“分配式排序”（distribution sort），
+又称“桶子法”（bucket sort）或bin sort，顾名思义，它是透过键值
+的部份资讯，将要排序的元素分配至某些“桶”中，藉以达到排序的作用，基
+数排序法是属于稳定性的排序，其时间复杂度为O (nlog(r)m)，其中r为所
+采取的基数，而m为堆数，在某些时候，基数排序法的效率高于其它的稳定性排序法。
+"""
+import math
+def radix_sorting(lists, radix=10):
+    k = int(math.ceil(math.log(max(lists), radix)))
+    bucket = [[] for i in range(radix)]
+    for i in range(1, k+1):
+        for j in lists:
+            bucket[j / (radix ** (i - 1)) % (radix ** i)].append(j)
+            del lists[:]
+            for z in bucket:
+                lists += z
+                del z[:]
+    return lists
